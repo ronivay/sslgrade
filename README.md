f4099501c910278824e018ec1f238db28c0d72ef
ronivay <roni@vayrynen.info>
2017-09-09 13:14:03 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # sslgrade
+
+
+This simple bash script will initiate [SSL Labs](https://www.ssllabs.com/ssltest/) test through API.
+
+Test will print following results:
+
+- Grade
+- Warnings boolean (true/false)
+- Possible issues in certificate chain 
+- Supported protocols
+
+Test will ignore certificate mismatch and fetch results anyway. T/M grade will indicate this happened.
+
+SSL Labs API info: https://github.com/ssllabs/ssllabs-scan/blob/stable/ssllabs-api-docs.md 
+
+Tests ran through API are not published in SSL Labs website by default.
+
+
+### Usage
+
+``` ./sslgrade.sh domain.tld ```
+
+
+### Requirements
+
+
+``` 
+curl
+jq 
+```
+
+
+### Notes
+
+This isn't official SSL Labs tool.
+Official CLI tool can be found: https://github.com/ssllabs/ssllabs-scan/
+
+Take care that you follow terms of use: https://www.ssllabs.com/downloads/Qualys_SSL_Labs_Terms_of_Use.pdf
---
+++ sslgrade.sh
@@ -0,0 +1,129 @@
+#/bin/bash
+
+DOMAIN="$1"
+
+if [[ $# == "0" ]]; then
+        echo "Usage $(dirname $0)/$(basename $0) domain.tld"
+        exit 0
+fi
+
+function DependencyCheck {
+
+if [[ -z $(which curl) ]]; then
+	echo "This script requires curl"
+	exit 0
+fi
+
+if [[ -z $(which jq) ]]; then
+	echo "This script require jq"
+	exit 0
+fi
+
+}
+
+function TestCheck {
+
+local STATUS=$(curl -s "https://api.dev.ssllabs.com/api/v2/analyze?host=$DOMAIN&publish=off&analyze" | jq ".endpoints[].statusMessage" 2>/dev/null | sed 's/\"//g' 2>/dev/null)
+
+if [[ "$STATUS" == "In progress" ]]; then
+	echo "There's a test In progress, try again later"
+	exit 0
+elif [[ "$STATUS" == "Ready" ]]; then
+	echo "There's existing fresh result"
+	echo "Start new test anyway?"
+	read -p "[y/N] " answer
+		case $answer in
+		
+		y)
+		StartTest
+		WaitForTest
+		TestResultPrint
+		exit 0
+		;;
+		n)
+		TestResultPrint
+		exit 0
+		;;
+	esac
+else
+	StartTest
+	WaitForTest
+	TestResultPrint
+fi
+
+}
+
+function StartTest {
+
+	echo 
+	echo "Calling API to start test"
+	echo "API reply:"
+	curl -s "https://api.dev.ssllabs.com/api/v2/analyze?host=$DOMAIN&publish=off&analyze&startNew=on&ignoreMismatch=on"
+	echo
+}
+	
+
+function WaitForTest {
+
+	sleep 5
+	local STATUS=$(curl -s "https://api.dev.ssllabs.com/api/v2/analyze?host=$DOMAIN&publish=off&analyze")
+	local PROGRESS=$(echo "$STATUS" | jq ".endpoints[].statusMessage" 2>/dev/null | sed 's/\"//g' 2>/dev/null)
+
+	if [[ $(echo "$STATUS" | jq ".status") =~ "ERROR" ]] || [[ $(echo "$STATUS" | jq ".errors") != "null" ]]; then
+		echo 
+		echo "Error initializing test"
+		exit 1
+	fi
+	
+	echo "Waiting for test to finish"
+	while [[ "$PROGRESS" != "Ready" ]]; do
+		sleep 3
+		local local STATUS=$(curl -s "https://api.dev.ssllabs.com/api/v2/analyze?host=$DOMAIN&publish=off&analyze")
+		local PERCENTAGE=$(echo "$STATUS" | jq ".endpoints[].progress" 2>/dev/null | sed 's/\"//g' 2>/dev/null | sed 's/-1/0/' 2>/dev/null)
+		local TEST=$(echo "$STATUS" | jq ".endpoints[].statusDetailsMessage" 2>/dev/null | sed 's/\"//g' 2>/dev/null)
+		echo -ne "\r$PERCENTAGE%"
+		local PROGRESS=$(echo "$STATUS" | jq ".endpoints[].statusMessage" 2>/dev/null | sed 's/\"//g' 2>/dev/null)
+	done
+		echo
+}
+
+function TestResultPrint {
+
+		local RESULT=$(curl -s "https://api.dev.ssllabs.com/api/v2/analyze?host=$DOMAIN&publish=off&analyze")
+                local GRADE=$(echo "$RESULT" | jq ".endpoints[].grade" | sed 's/\"//g')
+                local WARNINGS=$(echo "$RESULT" | jq ".endpoints[].hasWarnings")
+		local IPADDRESS=$(echo "$RESULT" | jq ".endpoints[].ipAddress" | sed 's/\"//g')
+		local DETAILS=$(curl -s "https://api.dev.ssllabs.com/api/v2/getEndpointData?host=$DOMAIN&s=$IPADDRESS")
+		local CERTCHAIN=$(echo "$DETAILS" | jq ".details.chain.issues" | sed 's/\"//g')
+		local PROTOCOLS=$(echo "$DETAILS" | jq ".details.protocols[].version" | sed 's/\"//g' | tr '\n' ' ')
+
+		echo
+                echo "SSL Labs test results for $DOMAIN"
+                echo "Grade: $GRADE"
+                echo "Warnings: "$WARNINGS""
+		echo
+#		echo "$CERTCHAIN"
+		echo -n "Issues in certficiate chain: "
+		if [[ $CERTCHAIN == "0" ]]; then
+			echo "none"
+		elif [[ $CERTCHAIN == "1" ]]; then
+			echo "incomplete chain"
+		elif [[ $CERTCHAIN == "2" ]]; then
+			echo "chain contains unrelated or duplicate certificates"
+		elif [[ $CERTCHAIN == "3" ]]; then
+			echo "the certificates form a chain (trusted or not), but the order is incorrect"
+		elif [[ $CERTCHAIN == "4" ]]; then
+			echo "contains a self-signed root certificate"
+		elif [[ $CERTCHAIN == "5" ]]; then
+			echo "the certificates form a chain, but we could not validate it"
+		fi
+#		echo "Issues in certificate chain: "$CERTCHAIN""
+		echo "Supported protocols: "$PROTOCOLS""
+		
+                echo
+                echo "See full report at https://www.ssllabs.com/ssltest/analyze.html?d=$DOMAIN&fromCache=on"
+
+}
+
+DependencyCheck
+TestCheck
